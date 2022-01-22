556d4626a481c9414f9e8392dcb1ca6753b87469
aobiegb <aobiegb@us.ibm.com>
2022-01-22 00:11:49 -0600

    auto generate customers

--- app/db/models.py
+++ app/db/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from datetime import datetime
 
 from app.db.session import Base
 
@@ -35,7 +36,7 @@ class Customer(Base):
     last_name = Column(String, nullable=True)
     creator_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=True, onupdate=datetime.utcnow)
 
     creator = relationship("User")
 
